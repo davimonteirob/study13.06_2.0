9c78c9e0e1569e6190e6bafd1cc222b6289a64e8
Davi Monteiro <davi321522@gmail.com>
2024-06-13 14:44:09 -0300

    study and practic

---
+++ study13.06_2.0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study13._06_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
